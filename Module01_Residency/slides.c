d43122f234b0dc4707991be030b1da6cf6c2170a
Albert Lionelle <albert.lionelle@colostate.edu>
2024-01-08 15:04:49 -0700

    added if example

--- Module01_Residency/slides.c
+++ Module01_Residency/slides.c
@@ -9,7 +9,24 @@
 #include <stdlib.h>
 
 
-void memory_stack_example() {
+int if_example(int val, int val2) {
+    if(val-val2 > 0) { // more common
+        return 1;
+    }
+    else if(val-val2 < 0) return -1; // single line is allowed
+    // else implied 
+    return 0;
+}
+
+void run_if_example() {
+    for(int i = 0; i < 10; i++) {
+        int val = rand() % 10;
+        int val2 = rand() %10;
+        printf("Val: %d, val2: %d, result %d\n", val, val2, if_example(val, val2));
+    }
+}
+
+void memory_example() {
     int x = 5;
     double y = 3.14;
 
@@ -22,5 +39,6 @@ void memory_stack_example() {
 
 int main() {
     memory_example();
+    run_if_example();   
     return 0;
 }

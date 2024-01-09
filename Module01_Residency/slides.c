d8839decebbc73e16ebd2fc2bd13345ca491fdaa
Albert Lionelle <albert.lionelle@colostate.edu>
2024-01-09 12:19:45 -0500

    added loop examples

--- Module01_Residency/slides.c
+++ Module01_Residency/slides.c
@@ -18,12 +18,31 @@ int if_example(int val, int val2) {
     return 0;
 }
 
+int loop_example(int counter) {
+    int rtn = 0;
+    // for loops are based on index
+    for(int i = 0; i < counter; i++) {
+        rtn += i + 1;
+    }
+    // this is the equivalent while loop
+    int j = 0;
+    while(j < counter) {
+        rtn += j + 1;
+        j++; // don't forget the increment, this is equivalent to j += 1;
+    }
+    return rtn;
+}
+
 void run_if_example() {
     for(int i = 0; i < 10; i++) {
         int val = rand() % 10;
         int val2 = rand() %10;
         printf("Val: %d, val2: %d, result %d\n", val, val2, if_example(val, val2));
     }
+}
+
+void run_loop_example() {
+    printf("Final value is: %d\n", loop_example(10));
 }
 
 void memory_example() {
@@ -40,5 +59,6 @@ void memory_example() {
 int main() {
     memory_example();
     run_if_example();   
+    run_loop_example();
     return 0;
 }
